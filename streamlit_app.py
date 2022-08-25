370434190f32672019107a1cf374f69d5ec3ac15
Sj-sanketjain <37847281+Sj-sanketjain@users.noreply.github.com>
2022-08-25 12:29:37 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,3 +6,6 @@ streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free Range Eggs')
+streamlit.text ('Avacado Toast')
+
+streamlit.header('Make your Own Smoothie')
