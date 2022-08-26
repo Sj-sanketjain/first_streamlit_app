e79b1fd59a5f3cc2854b06e135c64641ad159d1c
Sj-sanketjain <37847281+Sj-sanketjain@users.noreply.github.com>
2022-08-26 19:40:58 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,24 +13,26 @@ streamlit.text('Hard-Boiled Free Range Eggs')
 streamlit.text ('Avacado Toast')
 
 streamlit.header('Make your Own Smoothie')
-
 my_fruit_list=pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list=my_fruit_list.set_index('Fruit')
-
 fruits_selected= streamlit.multiselect("Pick Some Fruit:", list(my_fruit_list.index),['Avocado','Strawberries'])
-
 fruits_to_show = my_fruit_list.loc[fruits_selected]
-
 streamlit.dataframe(fruits_to_show)
 
+#New section to display fruityvice api response
 streamlit.header("Fruityvice Fruit Advice!")
+try:
+ fruit_choice = streamlit.text_input('What fruit would you like information about?')
+ if not fruit_choice:
+    streamlit.error("Please select a fruit to get information")
+ else:
+   fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
+   streamlit.write('The user entered ', fruit_choice)
+   fruityvice_normalized = pd.json_normalize(fruityvice_response.json())  # json to dataframe
+   streamlit.dataframe(fruityvice_normalized) # print dataframe
 
-# import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+"kiwi")
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
-fruityvice_normalized = pd.json_normalize(fruityvice_response.json())  # json to dataframe
-streamlit.dataframe(fruityvice_normalized) # print dataframe
+except URLError as e:
+ streamlit.error()
 
 streamlit.stop()
 # import snowflake.connector
